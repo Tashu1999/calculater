7b8a2c66a34d4fdfb989d7c94bb9d3b9e4b1c7db
unknown <tashujangid1999@gmail.com>
2023-03-31 15:21:56 +0530

    calculator

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function valueButton(e){
+    calculatorform.screen.value += e.value;
+}
+function Clear(){
+    calculatorform.screen.value = null;
+}
+
+function Calculate(){
+    calculatorform.screen.value = eval(calculatorform.screen.value)
+}
